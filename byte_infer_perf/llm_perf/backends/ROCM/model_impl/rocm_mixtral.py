730da2cce79a0257e6c7d272625365b9c5201e46
valarLip <340077269@qq.com>
2024-10-16 10:06:53 +0800

    code clean up

--- byte_infer_perf/llm_perf/backends/ROCM/model_impl/rocm_mixtral.py
+++ byte_infer_perf/llm_perf/backends/ROCM/model_impl/rocm_mixtral.py
@@ -146,14 +146,12 @@ class GPUMixtral(nn.Module):
         if self.mp_size > 1:
             set_custom_all_reduce(True)
 
-            print('init_distributed_environment')
             init_distributed_environment(
                 world_size=self.mp_size, 
                 rank=self.local_rank, 
                 distributed_init_method=get_distributed_init_method("127.0.0.1", get_open_port()))
                 # distributed_init_method=get_distributed_init_method(get_ip(), get_open_port()))
 
-            print('ensure_model_parallel_initialized')
             ensure_model_parallel_initialized(self.mp_size, 1)
             logger.info(f"RANK: {self.local_rank} {self.mp_size} init_process_group...")
             # dist.init_process_group(
